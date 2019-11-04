57edee3e6aa92e931b330a076a3999b248ea574a
Silenrate <daniwalteros596@gmail.com>
2019-11-04 11:11:21 -0500

    Pruebas Registro Recurso

--- src/test/java/edu/eci/cvds/test/ServiciosBiblioEciTest.java
+++ src/test/java/edu/eci/cvds/test/ServiciosBiblioEciTest.java
@@ -1,9 +1,6 @@
 package edu.eci.cvds.test;
 
 
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
-
 import java.text.SimpleDateFormat;
 import edu.eci.cvds.samples.entities.Recurso;
 import edu.eci.cvds.samples.services.ExcepcionServiciosBiblioEci;
@@ -12,6 +9,8 @@ import edu.eci.cvds.samples.services.ServiciosBiblioEciFactory;
 import org.junit.Test;
 import org.mybatis.guice.transactional.Transactional;
 import org.junit.After;
+
+import static org.junit.Assert.*;
 
 
 @Transactional
@@ -33,43 +32,63 @@ public class ServiciosBiblioEciTest {
 
     @Test
     public void shouldNotRegisterAResourceWithANullName() {
+        Recurso recurso = null;
         try {
-            serviciosBiblioEci.registrarRecurso(new Recurso(null, "lugarprueba", "Sala de estudio", 5,2));
+            recurso = new Recurso(null, "lugarprueba", "Sala de estudio", 5,2);
+            serviciosBiblioEci.registrarRecurso(recurso);
             fail("Se esperaba la excepcion por nombre nulo");
-        } catch (ExcepcionServiciosBiblioEci excepcionServiciosBiblioEci) {}
+        } catch (ExcepcionServiciosBiblioEci e) {
+			assertEquals(e.getMessage(),"Error al guardar el recurso "+recurso);
+		}
     }
 
     @Test
     public void shouldNotRegisterAResourceWithNullUbication() {
+        Recurso recurso = null;
         try {
-            serviciosBiblioEci.registrarRecurso(new Recurso("prueba", null, "Sala de estudio", 5,2));
+            recurso = new Recurso("prueba", null, "Sala de estudio", 5,2);
+            serviciosBiblioEci.registrarRecurso(recurso);
             fail("Se esperaba la excepcion por ubicacion nula");
-        } catch (ExcepcionServiciosBiblioEci excepcionServiciosBiblioEci) {}
+        } catch (ExcepcionServiciosBiblioEci e) {
+            assertEquals(e.getMessage(),"Error al guardar el recurso "+recurso);
+        }
     }
 
     @Test
     public void shouldNotRegisterAResourceWithNullType() {
+        Recurso recurso = null;
         try {
-            serviciosBiblioEci.registrarRecurso(new Recurso("prueba", "lugarprueba", null, 5,2));
+            recurso = new Recurso("prueba", "lugarprueba", null, 5,2);
+            serviciosBiblioEci.registrarRecurso(recurso);
             fail("Se esperaba la excepcion por tipo nulo");
-        } catch (ExcepcionServiciosBiblioEci excepcionServiciosBiblioEci) {}
+        } catch (ExcepcionServiciosBiblioEci e) {
+            assertEquals(e.getMessage(),"Error al guardar el recurso "+recurso);
+        }
     }
 
 	@Test
     public void shouldNotRegisterAResourceWithNegativeCapacity() {
+        Recurso recurso = null;
         try {
-            serviciosBiblioEci.registrarRecurso(new Recurso("prueba", "lugarprueba", "Sala de estudio", -1,2));
+            recurso = new Recurso("prueba", "lugarprueba", "Sala de estudio", -1,2);
+            serviciosBiblioEci.registrarRecurso(recurso);
             fail("Se esperaba la excepcion por capacidad negativa");
-        } catch (ExcepcionServiciosBiblioEci excepcionServiciosBiblioEci) {}
+        } catch (ExcepcionServiciosBiblioEci e) {
+            assertEquals(e.getMessage(),"El recurso "+recurso+"tiene una capacidad invalida");
+        }
     }
 	
 
     @Test
     public void shouldNotRegisterAResourceWithNoCapacity() {
+        Recurso recurso = null;
         try {
-            serviciosBiblioEci.registrarRecurso(new Recurso("prueba", "lugarprueba", "Sala de estudio", 0,2));
+            recurso = new Recurso("prueba", "lugarprueba", "Sala de estudio", 0,2);
+            serviciosBiblioEci.registrarRecurso(recurso);
             fail("Se esperaba la excepcion por capacidad cero");
-        } catch (ExcepcionServiciosBiblioEci excepcionServiciosBiblioEci) {}
+        } catch (ExcepcionServiciosBiblioEci e) {
+            assertEquals(e.getMessage(),"El recurso "+recurso+"tiene una capacidad invalida");
+        }
     }
 
 

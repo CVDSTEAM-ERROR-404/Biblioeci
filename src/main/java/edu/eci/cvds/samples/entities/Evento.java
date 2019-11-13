e12027d27ff702130338f407762d922861ce0acb
alejovasquero <alejo_704@hotmail.com>
2019-11-13 11:20:42 -0500

    Avance correción mapper reserva

--- src/main/java/edu/eci/cvds/samples/entities/Evento.java
+++ src/main/java/edu/eci/cvds/samples/entities/Evento.java
@@ -2,7 +2,41 @@ package edu.eci.cvds.samples.entities;
 
 public class Evento {
     int id;
-    Horario hora_inicio;
-    Horario hora_fin;
+    Horario horaInicio;
+    Horario horaFin;
     String estado;
+
+    public int getID(){
+        return id;
+    }
+
+    public void setID(int id){
+        this.id=id;
+    }
+
+    public Horario getHoraInicio(){
+        return horaInicio;
+    }
+
+    public void setHoraInicio(Horario horaInicio){
+        this.horaInicio=horaInicio;
+    }
+
+    public Horario getHoraFin(){
+        return horaFin;
+    }
+
+    public void setHoraFin(Horario horaFin){
+        this.horaFin=horaFin;
+    }
+
+    public String getEstado(){
+        return estado;
+    }
+
+    public void setEstado(String estado){
+        this.estado = estado;
+    }
+
+
 }
